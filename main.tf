611ea3bad4a255c80b39fb815259eee3e5abb9d8
Ikenwa Chukwunonso <nonsoikenwa@gmail.com>
2025-10-07 21:16:11 -0600

    New

--- main.tf
+++ main.tf
@@ -19,12 +19,20 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-example-bucket-${random_id.suffix.hex}"
-  acl = "private"
+  acl    = "private"
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "logs/"
+  }
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
+# Dedicated bucket for logs
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-tf-example-bucket-logs-${random_id.suffix.hex}"
+  acl    = "log-delivery-write"
 }
+
 
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.example.id
