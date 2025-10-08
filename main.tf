6c8398855b6aa1d12f579aa0c7e28840b4bd4375
Ikenwa Chukwunonso <nonsoikenwa@gmail.com>
2025-10-07 20:47:40 -0600

    New

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
-} 
+}
 provider "aws" {
   region = var.aws
   access_key = var.aws2
