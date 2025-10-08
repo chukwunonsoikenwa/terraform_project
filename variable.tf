26ce778ac1d2f75af85691d7f0e931ca51fb40f2
Ikenwa Chukwunonso <nonsoikenwa@gmail.com>
2025-10-07 19:14:29 -0600

    New

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "aws" {
+      description = "AWS region to deploy resources in"
+      type        = string
+      sensitive   = true
+    
+    }
+
+    variable "aws2" {
+      description = "Access key"
+      type        = string
+      sensitive   = true
+
+    }
+    variable "aws3" {
+      description = "Secret key"
+      type        = string
+      sensitive   = true
+    
+    }
+    variable "TF_VERSION" {
+      description = "Terraform version"
+      type        = string
+      default     = "1.5.7"
+    }
